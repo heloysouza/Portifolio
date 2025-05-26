a09a336598942a511ecd3bdc89989043e9d76de6
heloysouza <heloiltonsl@gmail.com>
2025-05-25 22:31:27 -0300

    Atualização de todos os codigos e imagem.

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const form = document.querySelector('form');
-  form.addEventListener('submit', function(e) {
+  form.addEventListener('submit', function (e) {
     e.preventDefault();  // Impede o envio normal do formulário
     alert('Obrigado pela mensagem! Entrarei em contato em breve.');
     form.reset();  // Reseta os campos do formulário
   });
 });
+
+// Função para menu
+
+let botao = document.querySelector('.botao-menu')
+let menu = document.querySelector('.menu')
+
+botao.addEventListener('click', () => {
+  // Adiciona ou remove a classe 'active' do menu e do botão
+ 
+  menu.classList.toggle('active')
+
+})
+
 
