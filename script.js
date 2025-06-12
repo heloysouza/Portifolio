77be62ff71f0241e4634309f3c7b823db6c9cce8
heloysouza <heloiltonsl@gmail.com>
2025-06-12 13:55:00 -0300

    foi add uma parte do formulario de contato

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   const form = document.querySelector('form');
-  form.addEventListener('submit', function (e) {
-    e.preventDefault();  // Impede o envio normal do formulário
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();  // Impede o envio normal do formulário
     alert('Obrigado pela mensagem! Entrarei em contato em breve.');
     form.reset();  // Reseta os campos do formulário
   });
