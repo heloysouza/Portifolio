e9a8cbb0823453fc5d543aafdc3a3fd3c5860188
heloysouza <heloiltonsl@gmail.com>
2025-06-13 08:34:32 -0300

    Foi add algumas linhas do index, imagem e uma pag obrigado. e o script.js foi retirado algumas linhas.

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const form = document.querySelector('form');
-  form.addEventListener('submit', function (event) {
-    event.preventDefault();  // Impede o envio normal do formulário
-    alert('Obrigado pela mensagem! Entrarei em contato em breve.');
-    form.reset();  // Reseta os campos do formulário
-  });
-});
 
 // Função para menu
 
 let botao = document.querySelector('.botao-menu')
 let menu = document.querySelector('.menu')
-
-botao.addEventListener('click', () => {
-  // Adiciona ou remove a classe 'active' do menu e do botão
- 
-  menu.classList.toggle('active')
-
-})
 
 
