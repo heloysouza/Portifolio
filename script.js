d337af7e3534a20dd3cb5f837dca727a47d0331e
heloysouza <heloiltonsl@gmail.com>
2025-06-13 09:47:39 -0300

    atualizei a img e script

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 
+
 // Função para menu
 
 let botao = document.querySelector('.botao-menu')
 let menu = document.querySelector('.menu')
+
+botao.addEventListener('click', () => {
+    // Adiciona ou remove a classe 'active' do menu e do botão
+
+    menu.classList.toggle('active')
+
+})
+
+
 
 
